f17e37878535e34d28e55681e4b3a39e270d5aad
poojashelke144 <poojashelke144@gmail.com>
2025-12-05 18:01:36 +0530

    Update asg_deploy.sh

--- app/scripts/asg_deploy.sh
+++ app/scripts/asg_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+# Make sure this is line 1, column 1
 echo "Starting Auto Scaling Group instance refresh..."
-# This command forces the ASG to terminate old instances and launch new ones
 aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG_NAME}
 echo "Instance refresh command issued to ASG: ${ASG_NAME}"
