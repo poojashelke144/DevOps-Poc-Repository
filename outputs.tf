104dccc376d59d4af94a01542e36255abb2d76d5
poojashelke144 <poojashelke144@gmail.com>
2025-12-05 14:48:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,29 +3,21 @@ output "alb_dns_name" {
   value       = aws_lb.flask_alb.dns_name
 }
 
-// FIX START: Add a data source and update the output block
-
 data "aws_instances" "app_instances" {
-  # Filter instances that belong to the ASG's launch template configuration
   filter {
     name   = "tag:Name"
     values = ["flask-docker-instance"]
   }
 
-  # Ensure we look up the IDs for the instances that are actually running
   instance_tags = {
     Name = "flask-docker-instance"
   }
 }
 
-
 output "instance_public_ips" {
   description = "Public IPs of all EC2 instances (for debugging via SSH)"
-  # Reference the data source to get the list of public IPs
   value       = data.aws_instances.app_instances.public_ips
 }
-
-// FIX END: End of changes
 
 output "ssh_command_prefix" {
   description = "SSH connection command prefix (use with the IPs above)"
